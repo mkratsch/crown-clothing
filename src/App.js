fd37df9d26ef442ce919889e56a8815e8a2213f1
Martin Kratsch <martin.kratsch.extern@bitmarck.de>
2021-11-18 15:02:17 +0100

    header added and links to home and shop

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 const HatsPage = () => (
@@ -12,6 +13,7 @@ const HatsPage = () => (
 function App() {
 	return (
 		<div className='App'>
+			<Header />
 			<Routes>
 				<Route exact path='/' element={<HomePage />} />
 				<Route path='/shop' element={<ShopPage />} />
