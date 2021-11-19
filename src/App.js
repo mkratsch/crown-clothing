b2fe4c66255a97acd800a0389d0c95ab2972154a
Martin Kratsch <martin.kratsch.extern@bitmarck.de>
2021-11-19 13:09:08 +0100

    Integrated firebase authentication with google

--- src/App.js
+++ src/App.js
@@ -5,18 +5,38 @@ import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth } from './firebase/firebase.utils';
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			currentUser: null,
+		};
+	}
+	unsubscribeFromAuth = null;
 
-function App() {
-	return (
-		<div className='App'>
-			<Header />
-			<Routes>
-				<Route exact path='/' element={<HomePage />} />
-				<Route path='/shop' element={<ShopPage />} />
-				<Route path='/signin' element={<SignInAndSignUpPage />} />
-			</Routes>
-		</div>
-	);
+	componentDidMount() {
+		this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+			this.setState({ currentUser: user });
+		});
+	}
+
+	componentWillUnmount() {
+		this.unsubscribeFromAuth();
+	}
+
+	render() {
+		return (
+			<div className='App'>
+				<Header currentUser={this.state.currentUser} />
+				<Routes>
+					<Route exact path='/' element={<HomePage />} />
+					<Route path='/shop' element={<ShopPage />} />
+					<Route path='/signin' element={<SignInAndSignUpPage />} />
+				</Routes>
+			</div>
+		);
+	}
 }
 
 export default App;
